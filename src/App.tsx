afc5f999f7a61436c786d63cefe6709098905dac
MatheusGalhani <matheus.galhani@hotmail.com>
2023-11-09 09:04:09 -0300

    feat: add eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const x = 1;
+
+const App: React.FC = () => {
+  const liveId = useMemo(() => {
+    if (x === 1 && liveId) {
+      return x;
+    }
+    return 2;
+  }, []);
+  return <div className="App">{liveId}</div>;
+};
 
 export default App;
